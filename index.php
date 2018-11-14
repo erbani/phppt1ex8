0e850410a4030dab3b2bb225d55b8470be592ebc
ERBANI <alexandre.erbani@live.fr>
2018-11-14 10:04:02 +0100

    exercice8

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>PHP Exercice 7 Partie 1</title>
+    </head>
+    <body>
+        <?php
+        $addition = 3+4;
+        $multiplication = 5*20;
+        $division = 45/5;
+        ?> 
+        <p>resultat additon <?= $addition?></p> 
+        <p>resultat multiplication <?= $multiplication ?></p>
+        <p>resultat division <?= $division ?></p>
+    </body>
+</html>
+<!--Exercice 8
+
+Créer 3 variables.
+Dans la première mettre le résultat de l'opération 3 + 4.
+Dans la deuxième mettre le résultat de l'opération 5 * 20.
+Dans la troisième mettre le résultat de l'opération 45 / 5.
+Afficher le contenu des variables.
+-->
